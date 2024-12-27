507fc914c1bcd73cd45164c7a0d1de37275598ce
Ridhesh Goti <50884851+rid181198@users.noreply.github.com>
2024-12-27 12:12:55 +0100

    Update __init__.py

--- stockpred/__init__.py
+++ stockpred/__init__.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///stocks.db'
